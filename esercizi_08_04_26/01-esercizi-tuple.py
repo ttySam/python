740c9e7134bd2c0eec9531aa58dc2245c603c095
ttySam <samu.nobilr@hotmail.it>
2026-04-08 17:06:01 +0200

    second commit

---
+++ esercizi_08_04_26/01-esercizi-tuple.py
@@ -0,0 +1,29 @@
+"""
+Autore: Samuel Nobile
+Data: 08/04/2026
+Titolo: Scrivere un programma per rimuovere l'n- esimo elemento da una tupla non vuota.
+"""
+
+
+##
+## Funzioni:
+##
+
+def rimuovi_elemento(tupla: tuple, indice: int) -> tuple:
+    """Funzione: rimuove un elemento da una tupla
+    Parametri formali:
+    tuple tupla -> tupla da modificare
+    int numero -> indice per la rimozione
+    Valore di ritorno:
+    tuple -> tupla senza l'elemento all'indice indicato"""
+    
+    
+
+    return
+
+# Sezione di input Dati
+# Inizializzazioni variabili
+tupla1 = (3, 4, 8, 0 , 44)
+# Elaborazione
+# Eventuali sotto processi di Elaborazione
+# Sezione di output
